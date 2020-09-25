b3a1241d3f795b0e174fc34c7e2079b5e7855027
Jan Ondruch <h.ondruch@seznam.cz>
2020-09-25 21:12:34 +0200

    changes in the mn

--- src/components/MobileNet.js
+++ src/components/MobileNet.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useState, useEffect } from 'react';
 import * as mn from '@tensorflow-models/mobilenet';
+import * as tf from '@tensorflow/tfjs';
 
 import './MobileNet.css';
 
@@ -10,11 +11,17 @@ import './MobileNet.css';
 // React Native supports them, as well (since 0.59).
 // You can still use the old solution, it is 100% bw compatible.
 
+// Functional Component or Stateless Component
+// (opposed to a Class Component or Stateful Component)
+// No "render()" used in these components.
+// (opposed to a Class Component, which must have it)
+// Use functional components as much as you can, because they are
+// easier to read, test, maintain.
 const MobileNet = () => {
 
-    // Declare a new state variable called "clsResult".
+    // Declare a new state variable called "clsImage".
     // Before 2018, we would need to write a constructor here and then change the
-    // state via "this.state.clsResult".
+    // state via "this.state.clsImage".
     //
     // Now, we just use the new useState hook, that lets us add React state to
     // function components.
@@ -22,11 +29,15 @@ const MobileNet = () => {
     // Why the square brackets? Array destructing syntax.
     //
     // We just pass an initial state to this function at its definition.
-    // "clsResult" is an argument, "setClsResult" is a function.
-    const [clsResult, setClsResult] = useState([]);
-
+    // "clsImage" is an argument, "setClsImage" is a function.
+    const [clsImage, setClsImage] = useState([]);
+    const [clsVideo, setClsVideo] = useState([]);
+    
+    
     // You can think of the useEffect Hook as componentDidMount, 
     // componentDidUpdate, and componentWillUnmount combined.
+    //
+    // Hooks can be used to replicated lifecycle behavior.
     //
     // By default, useEffect runs after every render -
     // "things that happen after render".
@@ -36,30 +47,65 @@ const MobileNet = () => {
     // Alternatively, we can add there an argument, on which the Hook
     // will be called again.
     useEffect(() => {
-        console.log('useEffect here!');
-        setUpForInterface();
+        setUpInterface();
     }, []);
+    
+    // Component vars
+    // Don't know any better know where to put them (use them as "attributes")
+    // I don't think putting them into state would be a good idea...
+    let net;
+    let webcam;
 
-    // Load the MobileNet model and make a prediction through the model on an image
-    const setUpForInterface = () => {
+    // Load the MobileNet model and call the loadWebCam function.
+    const setUpInterface = () => {
         
         console.log("Loading mobilenet...");
-        let net;
 
         // Create an scoped async function in the Hook
         // Load the net
         async function loadMobileNet() {
             net = await mn.load();
             console.log("Successfully loaded model!");
-            classifyNet();
-        }
-        loadMobileNet();
 
-        // Classify the image and save the results into the state
-        async function classifyNet() {
-            const imgEl = document.getElementById('mnimg');
-            const results = await net.classify(imgEl);
-            results.map(r => setClsResult(clsResult => [...clsResult, r]));
+            loadWebCam();
+        }
+
+        loadMobileNet();
+    };
+
+    // Load the web cam
+    async function loadWebCam() {
+        const webcamElement = document.getElementById('webcam');    // const is block-scoped
+        webcam = await tf.data.webcam(webcamElement);
+        console.log("Webcam is loaded!");
+
+        videoClassification(true);
+        classifyImage();
+    }
+
+    // Classify the image and save the results into the state
+    async function classifyImage() {
+        const imgEl = document.getElementById('mnimg');
+        const results = await net.classify(imgEl);
+        setClsImage(results);
+    }
+
+    // Video classification.
+    async function videoClassification() {
+        while(true) {
+            const img = await webcam.capture();
+            const result = await net.classify(img);
+
+            setClsVideo(result);
+            // result.map(r => setClsVideo(clsVideo => [...clsVideo, r]));
+            // console.log(result[0].className, result[0].probability);
+
+            // Dispose the tensor to release the memory.
+            img.dispose();
+
+            // Give some breathing room by waiting for the next 
+            // animation frame to fire.
+            await tf.nextFrame();
         }
     };
 
@@ -68,34 +114,52 @@ const MobileNet = () => {
         <div className="mobile-net">
             <div>
                 <h2>MobileNet</h2>
-                <img 
-                    id='mnimg' 
-                    src={process.env.PUBLIC_URL + '/images/MobileNetImage.jpg'} 
-                    alt="MobileNet image" />
-                <h4>Classification result</h4>
-                <div>
-                    {
-                        clsResult.length ?
-                            clsResult.map((val, index) => {
-                                return (
-                                    <div key={index} className="row">
-                                        <input
-                                            className="field field-x column"
-                                            value={val.className}
-                                            name="className"
-                                            data-index={index} />
-                                    
-                                        <input 
-                                            className="field field-y column"
-                                            value={val.probability}
-                                            name="probability"
-                                            data-index={index} />
-                                    </div>
-                                );
-                            })
-                            :
-                        <p>Classifying...</p>
-                    }
+                <div className="video-classification">
+                    <h3>Video image classification</h3>
+                    <video autoPlay playsInline muted id="webcam" width="224" height="224"></video>
+                    <div>
+                        {
+                            clsVideo.length ?
+                            <p>{clsVideo[clsVideo.length-1].className}</p>
+                                :
+                            <p>Classifying...</p>
+                        }
+                    </div>
+                </div>
+                <div className="image-classification">
+                    <h3>Image classification</h3>
+                    <img 
+                        id='mnimg' 
+                        src={process.env.PUBLIC_URL + '/images/MobileNetImage.jpg'} 
+                        alt="MobileNet image" />
+                    <h4>Classification result</h4>
+                    <div>
+                        {
+                            clsImage.length ?
+                                clsImage.map((val, index) => {
+                                    return (
+                                        <div key={index} className="row">
+                                            <span
+                                                className="field field-x column"
+                                                name="className"
+                                                data-index={index}>
+                                                {val.className}
+                                            </span>
+                                        
+                                            <span 
+                                                className="field field-y column"
+                                                value={val.probability}
+                                                name="probability"
+                                                data-index={index}>
+                                                {val.probability}
+                                            </span>
+                                        </div>
+                                    );
+                                })
+                                :
+                            <p>Classifying...</p>
+                        }
+                    </div>
                 </div>
             </div>
         </div>
