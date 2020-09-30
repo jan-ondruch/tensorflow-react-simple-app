6483cdf5643c9bdf211f0d4007b6d8de3aade79b
Jan Ondruch <h.ondruch@seznam.cz>
2020-09-30 13:18:32 +0200

    mobile appbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 
-import { AppBar, Toolbar, Typography, } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, Toolbar, Typography, IconButton, 
+    Menu, MenuItem, ListItemIcon, ListItemText, } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
 import { Link } from "react-router-dom"
 
+import ImageIcon from '@material-ui/icons/Image'
+import FunctionsIcon from '@material-ui/icons/Functions'
+import InfoIcon from '@material-ui/icons/Info'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -37,34 +42,125 @@ const useStyles = makeStyles((theme) => ({
             color: '#00FE00'
         }
     },
+    menuButton: {
+        position: 'absolute',
+        right: '12px'
+    },
+    mobileItemLink: {
+        textDecoration: 'none'
+    }
 }))
 
+const StyledMenu = withStyles({
+    paper: {
+      border: '1px solid #d3d4d5',
+    },
+  })((props) => (
+    <Menu
+      elevation={0}
+      getContentAnchorEl={null}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      {...props}
+    />
+))
+
+const StyledMenuItem = withStyles((theme) => ({
+    root: {
+      '&:focus': {
+        backgroundColor: theme.palette.primary.main,
+        '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+          color: theme.palette.common.white
+        },
+      },
+    },
+}))(MenuItem)
+
+
+
 const Navbar = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null)
+
     const classes = useStyles()
-    const desktopNavbar = useMediaQuery('(min-width:600px)')
+    const desktopNavbar = useMediaQuery('(min-width:668px)')
+
+    const handleClick = (event) => setAnchorEl(event.currentTarget)
+
+    const handleClose = () => setAnchorEl(null)
 
     return (
-        desktopNavbar &&    // show only on desktop
-        <div className={classes.root}>
-            <AppBar color="primary" title="TensorFlow React JS App" position="static">
-                <Toolbar>
-                    <Typography variant="h6">
-                        <Link to="/" className={classes.logo}>TR</Link>
-                    </Typography>
-                    <div className={classes.navRight}>
-                        <Typography variant="h6" className={classes.item}>
-                            <Link to="/" className={classes.link}>Object Recognition</Link>
+        desktopNavbar ?
+            <div className={classes.root}>
+                <AppBar color="primary" title="TensorFlow React JS App" position="static">
+                    <Toolbar>
+                        <Typography variant="h6">
+                            <Link to="/" className={classes.logo}>TR</Link>
                         </Typography>
-                        <Typography variant="h6" className={classes.item}>
-                            <Link to="/linear-regression" className={classes.link}>Linear Regression</Link>
+                        <div className={classes.navRight}>
+                            <Typography variant="h6" className={classes.item}>
+                                <Link to="/" className={classes.link}>Object Recognition</Link>
+                            </Typography>
+                            <Typography variant="h6" className={classes.item}>
+                                <Link to="/linear-regression" className={classes.link}>Linear Regression</Link>
+                            </Typography>
+                            <Typography variant="h6" className={classes.item}>
+                                <Link to="/about" className={classes.link}>About</Link>
+                            </Typography>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
+            :
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6">
+                            <Link to="/" className={classes.logo}>TR</Link>
                         </Typography>
-                        <Typography variant="h6" className={classes.item}>
-                            <Link to="/about" className={classes.link}>About</Link>
-                        </Typography>
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </div>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleClick}>
+                            <MenuIcon />
+                        </IconButton>
+                        <StyledMenu
+                            id="customized-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                        >
+                            <Link to="/" onClick={handleClose} className={classes.mobileItemLink}>
+                                <StyledMenuItem>
+                                    <ListItemIcon>
+                                        <ImageIcon fontSize="small" />
+                                    </ListItemIcon>
+                                    <ListItemText primary="Object Recognition" />
+                                </StyledMenuItem>
+                            </Link>
+                            <Link to="/linear-regression" onClick={handleClose} className={classes.mobileItemLink}>
+                                <StyledMenuItem>
+                                    <ListItemIcon>
+                                        <FunctionsIcon fontSize="small" />
+                                    </ListItemIcon>
+                                    <ListItemText primary="Linear Regression" />
+                                </StyledMenuItem>
+                            </Link>
+                            <Link to="/about" onClick={handleClose} className={classes.mobileItemLink}>
+                                <StyledMenuItem>
+                                    <ListItemIcon>
+                                        <InfoIcon fontSize="small" />
+                                    </ListItemIcon>
+                                    <ListItemText primary="About" />
+                                </StyledMenuItem>
+                            </Link>
+                        </StyledMenu>
+                    </Toolbar>
+                </AppBar>
+            </div>
     )
 }
 
