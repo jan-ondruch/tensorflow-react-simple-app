5959e476d502f48cb6ddc76263e086afd090fe7b
Jan Ondruch <h.ondruch@seznam.cz>
2020-10-09 07:43:44 +0200

    updated version

---
+++ precache-manifest.ebf486c93f62ee6c0acdc75ebaa0fefe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2afb1c397fc9eb6a01ae15e1310294",
+    "url": "./index.html"
+  },
+  {
+    "revision": "349bd1cbfb3f938a6e8d",
+    "url": "./static/css/main.282b7866.chunk.css"
+  },
+  {
+    "revision": "8c636f41978f0093e857",
+    "url": "./static/js/2.22305ac9.chunk.js"
+  },
+  {
+    "revision": "cf214fb58d6ea88c9cab86c5d4c422e4",
+    "url": "./static/js/2.22305ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349bd1cbfb3f938a6e8d",
+    "url": "./static/js/main.8f302ca0.chunk.js"
+  },
+  {
+    "revision": "99a5ec3c8539738c959a",
+    "url": "./static/js/runtime-main.5a8eaee9.js"
+  }
+]);
