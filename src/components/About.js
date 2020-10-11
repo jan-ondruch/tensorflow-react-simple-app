b152fd20475be219bb2890e320fb074b1f7578e4
Jan Ondruch <h.ondruch@seznam.cz>
2020-10-11 18:45:13 +0200

    material ui styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import Header from './Header'
 
-import { Typography, Link } from '@material-ui/core'
+import { Typography, Box, Link } from '@material-ui/core'
 
-const About = () => (
-    <React.Fragment>
-        <Header name="About" />
-        <Typography variant="subtitle1" color="secondary">
-            This project was based on
-                <Link
-                href="https://www.dlighthouse.co/2020/02/creating-tensorflowjs-reactjs-js-app.html"
-                color="primary"> this tutorial
-                </Link>
-        </Typography>
-        <Typography variant="subtitle1" color="secondary">
-            My personal contribution was addition of all the neat functionality around and styles.
-        </Typography>
-    </React.Fragment>
-)
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+    contentWrapper: {
+        textAlign: 'center',
+    },
+}))
+
+
+const About = () => {
+    
+    const classes = useStyles()
+
+    return (
+        <React.Fragment>
+            <Header name="About" />
+            <Box className={classes.contentWrapper}>
+                <Typography variant="subtitle1" color="secondary">
+                    This project was based on
+                        <Link
+                        href="https://www.dlighthouse.co/2020/02/creating-tensorflowjs-reactjs-js-app.html"
+                        color="primary"> this tutorial
+                        </Link>
+                </Typography>
+                <Typography variant="subtitle1" color="secondary">
+                    My personal contribution was addition of all the neat functionality around and styles.
+                </Typography>
+            </Box>
+        </React.Fragment>
+    )
+}
 
 export default About
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,18 @@
 import React from 'react'
-import { Box, Typography, Link, Divider } from '@material-ui/core'
-
-import { makeStyles } from '@material-ui/core/styles'
-
-const useStyles = makeStyles((theme) => ({
-  header: {
-    textAlign: 'center',
-    padding: '48px 32px'
-  },
-
-}))
+import { Box, Typography, Divider } from '@material-ui/core'
 
 const Header = ({ name }) => {
 
-    const classes = useStyles()
-
     return (
       <React.Fragment>
-        <Box className={classes.header}>
-            <Typography variant="h1" color="primary">
+        <Box
+           p={{ xs: 4, sm: 6, md: 7 }}
+           textAlign="center"
+        >
+            <Typography 
+              variant="h1" 
+              color="primary"
+            >
               {name}
             </Typography>
         </Box>
